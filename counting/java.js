7eb16dbc09cb6ba4289ff5ec2223484305534cb7
SindreKH <122084447+SindreKH@users.noreply.github.com>
2023-03-29 14:20:54 +0200

    Added counting test

---
+++ counting/java.js
@@ -0,0 +1,7 @@
+let count = new Array (100)
+    for(let i = 0; i < 100;i++){
+        count[i] = i+1;
+    }
+    for (let i = 0; i < count.length; i++) {
+            console.log(count[i]);
+            }
